46aeda4448f0e3817e2147822628c34e65903967
takeru.shingu <takeru.shingu@town.biz>
2025-07-29 23:25:49 +0900

    baseurlの追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isProd ? '/portfolio2025' : '',
+  assetPrefix: isProd ? '/portfolio2025/' : '',
+  trailingSlash: true,
+  // 他に必要なオプションをここに追加
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
-      { url: "/icon.svg", type: "image/svg+xml" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180" },
